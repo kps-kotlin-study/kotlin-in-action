e62163a4294d0506712e72cf956f3b4146adfc71
brett-20220015 <brett.bang@kakaopaysec.com>
2023-02-02 23:44:24 +0900

    test code 추가

--- agit-common/src/test/kotlin/com/project/agit/common/person/PersonServiceTest.kt
+++ agit-common/src/test/kotlin/com/project/agit/common/person/PersonServiceTest.kt
@@ -4,11 +4,14 @@ import com.project.agit.common.domain.person.Person
 import com.project.agit.common.person.constant.Location
 import com.project.agit.common.person.dto.PersonRequest
 import com.project.agit.common.property.PersonProperty
+import com.project.agit.common.test.IntegrationTest
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
+import io.mockk.clearAllMocks
 import io.mockk.every
 import io.mockk.mockk
 
+@IntegrationTest
 class PersonServiceTest : BehaviorSpec({
     val personRepository = mockk<PersonRepository>()
     val personProperty = mockk<PersonProperty>()
@@ -27,7 +30,7 @@ class PersonServiceTest : BehaviorSpec({
     fun findByNameOrNull(personName: String) = personRepository.findByNameOrNull(personName)
     fun getPersonInfo(personName: String) = personService.getPersonInfo(personName)
 
-    Given("사람 이름이 존재하는 경우"){
+    Given("사람 이름이 존재하는 경우") {
         val personName = "brett"
 
         When("사람 이름이 유저 목록에 있으면") {
@@ -49,5 +52,9 @@ class PersonServiceTest : BehaviorSpec({
                 getPersonInfo(personName) shouldBe null
             }
         }
+    }
+
+    afterContainer {
+        clearAllMocks()
     }
 })
